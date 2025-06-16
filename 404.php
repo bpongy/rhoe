293269a067df6e47b6dc788522a1b2125fd93c79
Benjamin PONGY <bpongy@axome.com>
2025-06-16 23:55:31 +0200

    404 template

--- 404.php
+++ 404.php
@@ -4,14 +4,14 @@ get_header();
 
 <header class="entry-header">
 	<h1 class="title">
-		<?php echo get_the_title(); ?>
+		<?php echo __('404: A glitch in the matrix.', 'rhoe'); ?>
 	</h1>
 </header>
 
 <div class="entry-content">
-	<?php
-	the_content();
-	?>
+	<p>
+		<?php echo __('The page you are looking for has been moved, deleted, or possibly never existed.', 'rhoe'); ?>
+	</p>
 </div>
 
 <?php
